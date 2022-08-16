12bb750973d058f0816d7006a74224560469049e
Ben Best <ben@ecoquants.com>
2022-08-16 15:25:03 +0000

    -> shinydashboard

--- oceano/ui.R
+++ oceano/ui.R
@@ -1,47 +1,50 @@
-shinyUI(fluidPage(
-  tags$head(tags$link(rel="stylesheet", type="text/css", href="styles.css")),
-  titlePanel("CalCOFI Oceanographic App"),
+dashboardPage(
+  dashboardHeader(
+    title="CalCOFI Oceanographic App"),
 
-  sidebarLayout(
+  dashboardSidebar(
+    selectInput(
+      "sel_var",
+      "Variable",
+      setNames(
+        d_vars$table_field,
+        d_vars$plot_label)),
+    sliderInput(
+      "sel_depth_range",
+      "Depth(m) range",
+      min = 0, max = ctdcast_depths$max,
+      step = 10,
+      value = c(0, ctdcast_depths$max)),
+    dateRangeInput(
+      "sel_date_range",
+      "Date range",
+      ctdcast_dates$min,
+      ctdcast_dates$max,
+      startview = "year"),
+    selectInput(
+      "sel_time_step",
+      "Time step (to summarize)",
+      c("decade",
+        "year",
+        "year.quarter","year.month","year.week",
+        "date",
+        # climatic
+        "quarter","month","week","julianday","hour"),
+      "year"),
+    selectInput(
+      "sel_stats",
+      "Statistics (to summarize)",
+      # c("average +/- standard deviation",
+      #   "average + 95% - 5%",
+      #   "average + maximum - minimum",
+      #   "median + 90% - 10%")) ),
+      c("avg +/- sd",
+        "avg +/- 45%",
+        "avg +/- max",
+        "median +/- 40%")) ),
     
-    sidebarPanel(
-      selectInput(
-        "sel_var",
-        "Variable",
-        setNames(
-          d_vars$table_field,
-          d_vars$plot_label)),
-      sliderInput(
-        "sel_depth_range",
-        "Depth(m) range",
-        min = 0, max = ctdcast_depths$max,
-        step = 10,
-        value = c(0, ctdcast_depths$max)),
-      dateRangeInput(
-        "sel_date_range",
-        "Date range",
-        ctdcast_dates$min,
-        ctdcast_dates$max,
-        startview = "year"),
-      selectInput(
-        "sel_time_step",
-        "Time step (to summarize)",
-        c("decade",
-          "year",
-          "year.quarter","year.month","year.week",
-          "date",
-          # climatic
-          "quarter","month","week","julianday","hour"),
-        "year"),
-      selectInput(
-        "sel_stats",
-        "Statistics (to summarize)",
-        c("average +/- standard deviation",
-          "average + 95% - 5%",
-          "average + maximum - minimum",
-          "median + 90% - 10%")) ),
-    
-    mainPanel(
+    dashboardBody(
+      tags$head(tags$link(rel="stylesheet", type="text/css", href="styles.css")),
       tabsetPanel(
         tabPanel(
           "Area",
@@ -79,7 +82,7 @@ shinyUI(fluidPage(
             sort(d_cruises$cruiseid, decreasing=T)),
           actionButton("btn_r", "Interpolate Variable from Cruise (within Depth range)"),
           br(),
-          leafletOutput("map_r"),
+          leafletOutput("map_r")
           # TODO: fix dl_tif, not working
           #downloadLink("dl_tif", "Download data (*.tif)")
         ),
@@ -87,4 +90,4 @@ shinyUI(fluidPage(
           "Depth",
           "TODO")
         ))
-  )))
+)
