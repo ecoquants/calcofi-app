7bf375acfa0f29c1aa9bb82d94acbf3453d1a9f9
Ben Best <ben@ecoquants.com>
2023-12-20 14:06:07 -0800

    + comments on othe explorations

--- hex_density/server.R
+++ hex_density/server.R
@@ -13,9 +13,13 @@ shinyServer(function(input, output, session) {
     # tj$vector_layers <- tj$vector_layers |>
     #   discard(~ .$id == "water")
     
+    # [Arc Layer : Dynamic changes on Click event · Issue #72 · qfes/rdeck](https://github.com/qfes/rdeck/issues/72#issuecomment-1150792238)
+    # const map = rdeck.getWidgetById("map");
+    # map.state.deckgl.onClick = (info, event) => { /* do something */ };
     rdeck(
-      map_style = mapbox_dark(),
-      theme = "light",
+      id             = "rdeck-map",
+      map_style      = mapbox_dark(),
+      theme          = "light",
       initial_bounds = st_bbox(
         c(xmin=-136, ymin=18, xmax=-106, ymax=50),
         crs = st_crs(4326)))  |> 
@@ -56,6 +60,56 @@ shinyServer(function(input, output, session) {
         tooltip            = c(castid, datetime),
         max_zoom           = 10,
         visibility_toggle  = T)
+    
+    # * [Place an MVT layer under a layer present in the basemap · Issue #58 · qfes/rdeck](https://github.com/qfes/rdeck/issues/58)
+    # rdeck(
+    #   map_style = "mapbox://styles/mapbox/streets-v11",
+    #   initial_view_state = view_state(
+    #     center = c(-73.58, 45.53), 
+    #     zoom = 10.1)) |> 
+    #   add_mvt_layer(
+    #     id = "test", 
+    #     data = mvt_url("mapbox.mapbox-streets-v8"),
+    #     pickable = TRUE, tooltip = TRUE, 
+    #     get_text = rlang::sym("name"),
+    #     get_fill_color = "#FF0000",
+    #     point_type = "text")
+    
+    
+    # [MVTs not showing on deployed website · Issue #78 · qfes/rdeck](https://github.com/qfes/rdeck/issues/78#issuecomment-1198756996)
+    # layers <- tibble::tibble(
+    #   count = c(988L, 1000L, 1000L, 1000L, 1000L, 1000L, 24L, 89L, 5L),
+    #   attribute = c("Bicycle (Baseline)", "Bicycle (Decarbonise)", "Bicycle (Demand reduction)", "Bicycle (Ebike)", "Bicycle (Go Dutch)", "Bicycle (Near market)", "Gradient", "Quietness", "col"),
+    #   type = c("number", "number", "number", "number", "number", "number", "number", "number", "string"),
+    #   min = c(1L, 1L, 1L, 1L, 1L, 1L, 0L, 3L, NA), 
+    #   max = c(2949L, 4020L, 4076L, 6493L, 4959L, 3447L, 46L, 100L, NA), 
+    #   values = list(NA_integer_, NA_integer_, NA_integer_, NA_integer_, NA_integer_, NA_integer_, NA_integer_, NA_integer_, c("#CC6677", "#882255", "#44AA99", "#117733", NA))
+    # )
+    # my_map <- rdeck(layer_selector = TRUE)
+    # add_bicycle_layer <- function(rdeck, layer_info, ...) {
+    #   rdeck |>
+    #     add_mvt_layer(
+    #       ...,
+    #       name = layer_info$attribute,
+    #       get_line_color = scale_color_power(
+    #         col = !!layer_info$attribute,
+    #         limits = layer_info$limits
+    #       )
+    #     )
+    # }
+    # my_map_with_layers <-
+    #   layers |>
+    #   dplyr::mutate(limits = cbind(min, max)) |>
+    #   dplyr::rowwise() |>
+    #   dplyr::group_split() |>
+    #   purrr::reduce(
+    #     add_bicycle_layer,
+    #     .init = my_map,
+    #     visible = FALSE,
+    #     # common params
+    #   )
+    # my_map_with_layers
+    
   })
   
 })
